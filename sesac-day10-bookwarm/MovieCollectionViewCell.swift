ebdc660a6ddc940f06c5d51860ef72c1f22236db
Gyeoul <clsmay.1st@gmail.com>
2023-08-01 20:15:45 +0900

    add Like Button & Action

--- sesac-day10-bookwarm/MovieCollectionViewCell.swift
+++ sesac-day10-bookwarm/MovieCollectionViewCell.swift
@@ -12,5 +12,20 @@ class MovieCollectionViewCell: UICollectionViewCell {
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var rateLabel: UILabel!
     @IBOutlet var posterImageView: UIImageView!
+    @IBOutlet var likeButton: UIButton!
     
+    func update(movie:Movie) {
+        titleLabel.textColor = .white
+        rateLabel.textColor = .white
+        likeButton.tintColor = .white
+        likeButton.setTitle("", for: .normal)
+        
+        if movie.like {
+            self.likeButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+            self.likeButton.setImage(UIImage(systemName: "heart"), for: .highlighted)
+        } else {
+            self.likeButton.setImage(UIImage(systemName: "heart"), for: .normal)
+            self.likeButton.setImage(UIImage(systemName: "heart.fill"), for: .highlighted)
+        }
+    }
 }
--- sesac-day10-bookwarm/MovieListCollectionViewController.swift
+++ sesac-day10-bookwarm/MovieListCollectionViewController.swift
@@ -14,6 +14,11 @@ class MovieListCollectionViewController: UICollectionViewController {
     let INSET_VALUE: CGFloat = 8
     let SPACING_VALUE: CGFloat = 0
     let ITEM_ROW: CGFloat = 2
+    var movie = MovieInfo() {
+        didSet {
+            collectionView.reloadData()
+        }
+    }
     @IBOutlet var navSearchButton: UIBarButtonItem!
     
     override func viewDidLoad() {
@@ -41,39 +46,43 @@ class MovieListCollectionViewController: UICollectionViewController {
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return MovieInfo().movie.count
+        return movie.movie.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? MovieCollectionViewCell else {
             return UICollectionViewCell()
         }
-        let this = MovieInfo().movie[indexPath.row]
+        let this = movie.movie[indexPath.row]
+        cell.update(movie: this)
         cell.posterImageView.image = UIImage(named: this.title)
         cell.bgView.layer.cornerRadius = 12
-        cell.bgView.backgroundColor = getRandomColor()
         cell.rateLabel.text = "\(this.rate)"
         cell.titleLabel.text = this.title
-        cell.rateLabel.textColor = .white
-        cell.titleLabel.textColor = .white
+        cell.likeButton.tag = indexPath.row
+        cell.likeButton.addTarget(self, action: #selector(heartButtonClick), for: .touchUpInside)
         return cell
     }
 
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let this = MovieInfo().movie[indexPath.row]
+        let this = movie.movie[indexPath.row]
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let vc: DetailViewController = storyboard.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
         vc.modalTransitionStyle = .coverVertical
         vc.modalPresentationStyle = .fullScreen
         vc.args = this
-        
-        //        present(vc, animated: true)
         navigationController?.pushViewController(vc, animated: true)
     }
+    
     func getRandomColor() -> UIColor {
         let randomRed = CGFloat(drand48() * 0.8 + 0.1)
         let randomGreen = CGFloat(drand48() * 0.8 + 0.1)
         let randomBlue = CGFloat(drand48() * 0.8 + 0.1)
         return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+    }
+    
+    @objc
+    func heartButtonClick(_ sender:UIButton) {
+        movie.movie[sender.tag].like.toggle()
     }
 }
